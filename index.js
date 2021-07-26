855545e9b85b2f1d4ae98fa6211a50a4be42477c
Gaurav Sangewar <70481952+techrider27@users.noreply.github.com>
2021-07-26 17:31:57 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+clock = () => {
+  let date = new Date();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let ampm = "AM";
+  // am pm
+  if (hours > 12) {
+    ampm = "PM";
+  }
+
+  // 12 hr format
+  if (hours == 0) {
+    hours = 12;
+  }
+  if (hours > 12) {
+    hours = hours - 12;
+  }
+
+  // 00:00:00
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+  document.getElementById("amorpm").innerHTML = ampm;
+  setTimeout(clock, 500);
+};
+clock();
